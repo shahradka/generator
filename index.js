2fe7da9303371a552433655f83c3dd03123ebf97
Shahrad Kashani <kashani.sh@tiddev.com>
2020-06-21 09:50:52 +0430

    inital commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const process = require('process');
+
+module.exports = function (plop) {
+    // react seam app generator
+    plop.setGenerator('createapp', {
+        description: 'create app generator',
+        prompts: data =>{
+            const prompts =
+            [
+                {
+                    type: 'input',
+                    name: 'projectName',
+                    message: 'please enter app name'
+                },
+                {
+                    type: 'input',
+                    name: 'projectHtmlTitle',
+                    message: 'please enter app html title'
+                },
+                {
+                    type: 'input',
+                    name: 'projectDescribtion',
+                    message: 'please enter app describtions'
+                },
+                {
+                    type: 'confirm',
+                    name: 'wantGitRepo',
+                    message: 'do you want to create git repository?',
+                    default:false
+                }
+            ],
+            if(data.wantGitRepo)
+            {
+                prompts.push(
+                    {
+                        type: 'input',
+                        name: 'projectGitUrl',
+                        message: 'please enter app Git Url'
+                    }
+                    )
+            }
+            return [...prompts,
+                {
+                    type: 'input',
+                    name: 'projectLicense',
+                    message: 'please enter app license'
+                },
+                {
+                    type: 'input',
+                    name: 'projectDefaultPort',
+                    message: 'please enter app default server port'
+                }
+            ]
+
+        },
+        actions: data => {
+                const actions = [
+                    {
+                        type: 'add',
+                        path: `app/index.html`,
+                        templateFile: 'templates/index.html.hbs'
+                    },
+                    {
+                        type: 'add',
+                        path: `server/port.js`,
+                        templateFile: 'templates/port.js.hbs'
+                    },
+                    {
+                        type: 'add',
+                        path: `package.json`,
+                        templateFile: 'templates/package.json.hbs'
+                    },
+                    {
+                        type: 'addMany',
+                        destination: process.cwd(),
+                        base:'templates/sources',
+                        globOptions:{
+                            dot:true
+                        },
+                        templateFiles: 'templates/sources/**'
+                    },
+                ]
+                return actions;
+            }
+    });
+};
---
+++ templates/sources/README.md
@@ -0,0 +1,22 @@
+<div align="center"><strong>Start your react project as easy as possible</strong></div>
+<div align="center">A highly scalable, offline-first foundation with the best DX and a focus on performance and best practices</div>
+<div align="center">
+  <sub>Created by <a href="mail:kashani.sh@tiddev.ir">k.Ahmadi & Sh.kashani </a> and maintained by an amazing <a href="https://github.com/orgs/react-boilerplate/people">team of developers</a>.</sub>
+</div>
+
+## Quick start
+
+1. in termial run this command
+> npm run generate.
+2. then set your git repository address
+> git remote set-url `<yourBranchName>` `<your repository URL>`
+3. then run
+> npm start
+4. happy coding!
+
+## Contributors
+
+## License
+
+This project is licensed under the MIT license, Copyright (c) 2019 Maximilian
+Stoiber. For more information see `LICENSE.md`.
