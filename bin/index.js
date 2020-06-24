8b12e688bc357ce0eda83523a17e4f19559a91dd
Shahrad Kashani <kashani.sh@tiddev.com>
2020-06-24 13:30:32 +0430

    change logo styles

--- bin/index.js
+++ bin/index.js
@@ -5,13 +5,27 @@ const args = process.argv.slice(2);
 const {Plop, run} = require('plop');
 const argv = require('minimist')(args);
 const CFonts = require('cfonts');
-CFonts.say('React-Seam App Generator', {
+CFonts.say('React-Seam |--------------', {
+    font: 'tiny',              // define the font face
+    align: 'left',              // define text alignment
+    colors: ['system'],         // define all colors
+    background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+    letterSpacing: 1,           // define letter spacing
+    lineHeight: 0.0,              // define the line height
+    space: true,                // define if the output text should have empty lines on top and on the bottom
+    maxLength: '0',             // define how many character can be on one line
+    gradient: false,            // define your two gradient colors
+    independentGradient: false, // define if you want to recalculate the gradient for each new line
+    transitionGradient: false,  // define if this is a transition between colors directly
+    env: 'node'                 // define the environment CFonts is being executed in
+});
+CFonts.say('[ App Generator ]', {
     font: 'simple',              // define the font face
     align: 'left',              // define text alignment
     colors: ['system'],         // define all colors
     background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
     letterSpacing: 1,           // define letter spacing
-    lineHeight: 1,              // define the line height
+    lineHeight: 0.0,              // define the line height
     space: true,                // define if the output text should have empty lines on top and on the bottom
     maxLength: '0',             // define how many character can be on one line
     gradient: false,            // define your two gradient colors
